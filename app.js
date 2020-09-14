9f2e4f2e783babe0c11ff529292940550c45f1d2
vin-jaehong <dnflwoghddl@gmail.com>
2020-09-14 21:06:30 +0900

    join_passporting_incomplete

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 var router = require("./router/index.js");
+var passport = require("passport");
+var localStrategy = require("passport-local").Strategy;
+var session = require("express-session");
+var flash = require("connect-flash");
 
-app.listen(3000,(req,res)=>
+
+app.listen(3000,"0.0.0.0",(req,res)=>
 {
     console.log("server connected...!");
 });
@@ -11,6 +16,16 @@ app.listen(3000,(req,res)=>
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("./public"));
+// passport 
+app.use(session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true    
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
 app.set("view engine","ejs");
 app.set("views","./views");
 
--- router/join.js
+++ router/join.js
@@ -1,14 +1,86 @@
 var express = require("express");
 var router = express.Router();
 var requestIp = require("request-ip");
+var mariaDB = require("mariadb");
+var moment = require("moment");
+const passport = require("passport");
+const { query } = require("express");
+var localStrategy = require("passport-local").Strategy;
+
+
+var pool = mariaDB.createPool({
+    host    : "localhost",
+    user    : "root",
+    password: "ki040504",
+    port    : 3306,
+    database: "location_based_sns"
+});
+pool.getConnection();
+
 
 router.get("/",(req,res)=>
 {
-    res.render("join.ejs");
+    var msg;
+    var errMsg = req.flash("error");
+    if(errMsg) msg = errMsg;
+    res.render("join.ejs",{"errMsg":msg});
 });
-router.post("/",(req,res)=>
+
+passport.serializeUser((user,done)=>
 {
-    var user = req.body;
+    console.log(user.name);
+});
+
+
+passport.use("local-join", new localStrategy(
+    {
+        usernameField : "id",
+        passwordField : "pw",
+        passReqToCallback : true
+    },
+    (req,id,pw,done)=>
+    {
+        pool.query(`select * from user where ID ="${id}";`).then((rows)=>
+        {
+            // 입력한 ID가 중복되는 경우
+            if(rows.length)
+            {
+                return done(null,false,{message:"ID가 중복됩니다"});
+            }
+            // 입력한 ID가 중복되지 않을 경우
+            const user = req.body;
+            const userLastLoginIp = req.headers['x-forwarded-for'] ||  req.connection.remoteAddress;
+            const userJoinDate = moment().format("YYYY-MM-DD");
+            const userFirstLoginDate = moment().format("YYYY-MM-DD HH:mm:ss");
+            
+            
+            
+            const query = `insert into user(NAME,GENDER,ID,PW,EMAIL,TEL,LAST_LOGIN_IP,JOIN_DATE,FIRST_LOGIN_DATE,AGREEMENT_01,AGREEMENT_02)
+            VALUES("${user.name}","${user.gender}","${user.id}","${user.pw}","${user.email}","${user.tel}",
+            "${userLastLoginIp}","${userJoinDate}","${userFirstLoginDate}","${user.agreement_01}","${user.agreement_02}");
+            `;
+            
+            pool.query(query).then((rows)=>
+            {
+                // 쿼리문 실행 완료 후
+                return done(null,{"id":id,"name":user.name});
+
+            }).catch((err)=>{throw err;});
+            
+
+        }).catch((err)=>{throw err;})
+    }
+));
+
+router.post("/", passport.authenticate("local-join",
+{
+    successRedirect : "/",
+    failureRedirect : "/join",
+    failureFlash : true
+}));
+
+/*
+const user = req.body;
     /*
     user.name
     user.gender
@@ -16,12 +88,10 @@ router.post("/",(req,res)=>
     user.pw
     user.email
     user.tel
-    */
-   const ip = req.headers['x-forwarded-for'] ||  req.connection.remoteAddress;
-
-    res.send(ip);
-
+    user.agreement_01
+    user.agreement_02
     
-});
+    
+ */   
 
 module.exports = router;
