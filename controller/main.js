c38b8142031923745b19ab2917c05960dc2d04c1
vin-jaehong <dnflwoghddl@gmail.com>
2020-12-19 13:37:01 +0900

    port

--- controller/main.js
+++ controller/main.js
@@ -1,6 +1,6 @@
 
 exports.mainView = (req,res)=>
 {
-    if(!req.user) res.render("main.ejs");
+    if(!req.user) res.render("main2.ejs");
     else res.render("main_user.ejs",{"userName":req.user});
 };
